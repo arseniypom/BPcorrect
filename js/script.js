3c67f0ac2343fc594e95ea20c32c7da61e62939c
arseniy_pomazkov <arpomazkov@bk.ru>
2020-07-27 22:19:40 +0300

    New style for footer

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var id = $(this).attr('href');
     var top = $(id).offset().top;
     $('body,html').animate({
-      scrollTop: top-60
+      scrollTop: top - 60
     }, 700);
   });
 });
@@ -80,43 +80,43 @@ toggle_video_modal();
 
 
 $(function() {
-// init the validator
-// validator files are included in the download package
-// otherwise download from http://1000hz.github.io/bootstrap-validator
+  // init the validator
+  // validator files are included in the download package
+  // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-$("#contact-form").validator();
+  $("#contact-form").validator();
 
-// when the form is submitted
-$("#contact-form").on("submit", function(e) {
-  // if the validator does not prevent form submit
-  if (!e.isDefaultPrevented()) {
-    var url = "contact.php";
+  // when the form is submitted
+  $("#contact-form").on("submit", function(e) {
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+      var url = "contact.php";
 
-    // FOR CODEPEN DEMO I WILL PROVIDE THE DEMO OUTPUT HERE, download the PHP files from
-    // https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
+      // FOR CODEPEN DEMO I WILL PROVIDE THE DEMO OUTPUT HERE, download the PHP files from
+      // https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
 
-    var messageAlert = "alert-success";
-    var messageText =
-      "Your message was sent, thank you. I will get back to you soon.";
+      var messageAlert = "alert-success";
+      var messageText =
+        "Your message was sent, thank you. I will get back to you soon.";
 
-    // let's compose Bootstrap alert box HTML
-    var alertBox =
-      '<div class="alert ' +
-      messageAlert +
-      ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-      messageText +
-      "</div>";
+      // let's compose Bootstrap alert box HTML
+      var alertBox =
+        '<div class="alert ' +
+        messageAlert +
+        ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+        messageText +
+        "</div>";
 
-    // If we have messageAlert and messageText
-    if (messageAlert && messageText) {
-      // inject the alert to .messages div in our form
-      $("#contact-form").find(".messages").html(alertBox);
-      // empty the form
-      $("#contact-form")[0].reset();
+      // If we have messageAlert and messageText
+      if (messageAlert && messageText) {
+        // inject the alert to .messages div in our form
+        $("#contact-form").find(".messages").html(alertBox);
+        // empty the form
+        $("#contact-form")[0].reset();
+      }
+
+      return false;
     }
-
-    return false;
-  }
-});
+  });
 
 });
